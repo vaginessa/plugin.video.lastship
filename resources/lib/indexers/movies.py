223cbe9bc69951d11d79c4e218d03643959d303f
Noctix <Elementarius@gmx.net>
2018-11-07 22:14:46 +0100

    Translation

--- resources/lib/indexers/movies.py
+++ resources/lib/indexers/movies.py
@@ -947,7 +947,7 @@ class movies:
                 cm = []
 
                 cm = []
-                cm.append(('Find similar', 'ActivateWindow(10025,%s?action=movies&url=https://api.trakt.tv/movies/%s/related,return)' % (sysaddon, imdb)))
+                cm.append(("Finde Ähnliches", 'ActivateWindow(10025,%s?action=movies&url=https://api.trakt.tv/movies/%s/related,return)' % (sysaddon, imdb)))
                 cm.append((queueMenu, 'RunPlugin(%s?action=queueItem)' % sysaddon))
                 
                 try:

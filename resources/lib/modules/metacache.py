1add28ed6249ce488a02ef5c7b8a9486431192db
Noctix <Elementarius@gmx.net>
2018-11-11 23:38:25 +0100

    metacache.py update
    Umstellung auf deutsch.

--- resources/lib/modules/metacache.py
+++ resources/lib/modules/metacache.py
@@ -30,7 +30,7 @@ except: from pysqlite2 import dbapi2 as database
 from resources.lib.modules import control
 
 
-def fetch(items, lang='en', user=''):
+def fetch(items, lang='de', user=''):
     try:
         t2 = int(time.time())
         dbcon = database.connect(control.metacacheFile)
@@ -68,7 +68,7 @@ def insert(meta):
         for m in meta:
             try:
                 if not "user" in m: m["user"] = ''
-                if not "lang" in m: m["lang"] = 'en'
+                if not "lang" in m: m["lang"] = 'de'
                 i = repr(m['item'])
                 try: dbcur.execute("DELETE * FROM meta WHERE (imdb = '%s' and lang = '%s' and user = '%s' and not imdb = '0') or (tvdb = '%s' and lang = '%s' and user = '%s' and not tvdb = '0')" % (m['imdb'], m['lang'], m['user'], m['tvdb'], m['lang'], m['user']))
                 except: pass

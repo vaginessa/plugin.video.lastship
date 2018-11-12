14cd6e9ed1c67a4df04e1c3f5ba3f9ccf7d4b55b
Noctix <Elementarius@gmx.net>
2018-11-12 20:27:09 +0100

    libtools.py update
    Option für deutsche Datei- und Ordnerstruktur.

--- resources/lib/modules/libtools.py
+++ resources/lib/modules/libtools.py
@@ -3,20 +3,21 @@
 """
     Lastship Add-on (C) 2019
     Credits to Placenta and Covenant; our thanks go to their creators
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+# Addon Name: Lastship
+# Addon id: plugin.video.lastship
+# Addon Provider: LastShip
 
 
 try:
@@ -631,5 +632,3 @@ class libepisodes:
                 pass
 
             control.sleep(10000)
-
-

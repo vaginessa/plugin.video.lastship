9d7ab965b8515b7193b443716a44c73ed3979970
Lastship <lastship@users.noreply.github.com>
2018-11-24 14:38:02 +0100

    [navigator] icon änderung

--- resources/lib/indexers/navigator.py
+++ resources/lib/indexers/navigator.py
@@ -54,7 +54,7 @@ class navigator:
     LOCALNEWS = os.path.join(THISADDONPATH, 'whatsnew.txt')
 
     def root(self):
-        self.addDirectoryItem('[COLOR=lime]Infos und Updates[/COLOR]', 'newsNavigator', 'library_update.png', 'DefaultAddonProgram.png') #news_paper.png sieht vor allem mit Exuary-Skin von Lastship nicht gut aus.
+        self.addDirectoryItem('[COLOR=lime]Infos und Updates[/COLOR]', 'newsNavigator', 'news_paper.png', 'DefaultAddonProgram.png') #news_paper.png sieht vor allem mit Exuary-Skin von Lastship nicht gut aus.
         self.addDirectoryItem("Suche", 'searchNavigator', 'search.png', 'DefaultFolder.png')
         self.addDirectoryItem("Filme", 'movieNavigator', 'movies.png', 'DefaultMovies.png')
         self.addDirectoryItem("TV-Serien", 'tvNavigator', 'tvshows.png', 'DefaultTVShows.png')

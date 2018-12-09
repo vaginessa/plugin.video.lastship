1a37132781ab55224677854130a2ccff23d6dcf7
merlux <givetrail@yahoo.de>
2018-12-09 18:57:25 +0100

    removed duckduckgo-search

--- resources/lib/modules/duckduckgo.py
+++ resources/lib/modules/duckduckgo.py
@@ -38,30 +38,8 @@ def cleanhtml(raw_html):
 
 
 def search(titles, year, site, titleRegex):
-    try:
-        params = {
-            'q': '%s site:%s' % (titles[0], site),
-            's': '0'
-        }
 
-        result = client.request(base_url, post=params, headers={'Content-Type': 'application/x-www-form-urlencoded','Referer':'https://duckduckgo.com/','Origin':'https://duckduckgo.com'}, error=True)
-        if cleantitle.get(params['q'].lower()) in cleantitle.get(result.lower()):
-            links = dom_parser.parse_dom(result, 'h2', attrs={'class': 'result__title'})
-            links = dom_parser.parse_dom(links, 'a')
-            links = [(client.replaceHTMLCodes(i.content), i.attrs['href']) for i in links if site in i.attrs['href']]
+    #DuckDuckGO should not be used with direct request.
+    #API is currently not useable for streamsearch, update when useable
 
-            links = [(re.findall(titleRegex, i[0])[0], i[1]) for i in links if re.search(titleRegex, i[0]) is not None]
-            links = [(cleanhtml(i[0]), i[1], year in i[1]) for i in links]
-
-            links = sorted(links, key=lambda i: i[2], reverse=True)  # with year > no year
-
-            t = [cleantitle.get(i) for i in set(titles) if i]
-
-            links = [i[1] for i in links if cleantitle.get(i[0]) in t]
-
-            if len(links) > 0:
-                return source_utils.strip_domain(links[0])
-
-        return
-    except:
-        source_faultlog.logFault(site, source_faultlog.tagSearch, titles[0] + '&' + site)
+    return
